1550d5c88cf3d1c0f4ba327c3646c612ed9c3bb4
unionman <zhifeng.zeng@unionman.com.cn>
2020-06-04 20:33:16 +0800

    first commit

---
+++ src/test/java/com/unionman/springbootfastdfs/SpringbootFastdfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unionman.springbootfastdfs;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFastdfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
